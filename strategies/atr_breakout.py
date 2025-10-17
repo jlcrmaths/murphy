6a6a29faca0b7d0af3a3245779d78bffaf5f15e6
jlcrmaths <145976617+jlcrmaths@users.noreply.github.com>
2025-10-17 13:31:05 +0200

    Update atr_breakout.py

--- strategies/atr_breakout.py
+++ strategies/atr_breakout.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import numpy as np
 
 def generate_signal(df):
     """
-    Estrategia de ruptura basada en ATR
-    - BUY si cierre rompe máximo de n días + volatilidad ATR
-    - SELL si cierre rompe mínimo de n días - ATR
+    Señal basada en ruptura de rango con ATR:
+        - BUY si close rompe máximo previo + 1 ATR
+        - SELL si close rompe mínimo previo - 1 ATR
     """
-    n = 14
     df = df.copy()
-    df['high_max'] = df['high'].rolling(n).max()
-    df['low_min'] = df['low'].rolling(n).min()
-    df['tr'] = df['high'] - df['low']
-    df['atr'] = df['tr'].rolling(n).mean()
-    
+    high, low, close = df['high'], df['low'], df['close']
+    df['tr'] = np.maximum.reduce([high - low,
+                                  abs(high - close.shift()),
+                                  abs(low - close.shift())])
+    df['atr14'] = df['tr'].rolling(14).mean()
     latest = df.iloc[-1]
+
     signal = {}
-    if latest['close'] > latest['high_max'] - 0.5*latest['atr']:
-        signal = {'signal':'BUY', 'reason':'Ruptura de máximo + ATR'}
-    elif latest['close'] < latest['low_min'] + 0.5*latest['atr']:
-        signal = {'signal':'SELL', 'reason':'Ruptura de mínimo - ATR'}
+    if latest['close'] > df['high'].shift(1) + latest['atr14']:
+        signal = {'signal': 'BUY', 'reason': 'Ruptura de máximo + ATR', 'color': 'green'}
+    elif latest['close'] < df['low'].shift(1) - latest['atr14']:
+        signal = {'signal': 'SELL', 'reason': 'Ruptura de mínimo - ATR', 'color': 'red'}
     return signal
